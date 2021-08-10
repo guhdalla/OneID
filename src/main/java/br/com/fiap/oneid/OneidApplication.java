8d03632afad529c866e32ce1ba251c17efd5f220
guhdalla <guhdalla011@gmail.com>
2021-08-10 08:50:07 -0300

    Feat: Estrutura do projeto

---
+++ src/main/java/br/com/fiap/oneid/OneidApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.oneid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneidApplication.class, args);
+	}
+
+}
