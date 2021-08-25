e15dd941c1bcca45869619a5f3bdbf3ca72f1f86
Bruuno96 <brunotkd96@gmail.com>
2021-08-25 19:34:57 -0300

    UF - UJ REPOSITORY and CRUD

--- src/main/java/br/com/fiap/oneid/controller/api/ApiUsuarioFisicoController.java
+++ src/main/java/br/com/fiap/oneid/controller/api/ApiUsuarioFisicoController.java
@@ -1,15 +1,20 @@
 package br.com.fiap.oneid.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.oneid.model.Usuario;
 import br.com.fiap.oneid.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/api/usuario")
-public class ApiUsuarioController {
+public class ApiUsuarioFisicoController {
 
 	@Autowired
 	private UsuarioRepository repository;
-}
+	
+	
+}		
