6effb0b0ba6360a13042f227eedd9b8135bc1eb9
guhdalla <guhdalla011@gmail.com>
2021-10-24 00:59:01 -0300

    Feat: Package .war

--- src/main/java/br/com/fiap/oneid/OneidApplication.java
+++ src/main/java/br/com/fiap/oneid/OneidApplication.java
@@ -3,15 +3,22 @@ package br.com.fiap.oneid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableSwagger2
-public class OneidApplication {
+public class OneidApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneidApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OneidApplication.class);
 	}
 }
