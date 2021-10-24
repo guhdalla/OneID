5caca00a2357f01e3aaac149014cecac85e5429a
guhdalla <guhdalla011@gmail.com>
2021-10-24 02:08:16 -0300

    Feat: Web configure SpringSecurity

--- src/main/java/br/com/fiap/oneid/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/oneid/config/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		super.configure(web);
+		web.ignoring().antMatchers("/resources/**");
 	}
 
 }
