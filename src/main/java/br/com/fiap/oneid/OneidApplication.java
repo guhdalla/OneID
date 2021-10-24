d094941b8fb936d1eed55d8dae1b9bf5dcd4c435
guhdalla <guhdalla011@gmail.com>
2021-10-24 01:41:41 -0300

    Fix: SpringAplication

--- src/main/java/br/com/fiap/oneid/OneidApplication.java
+++ src/main/java/br/com/fiap/oneid/OneidApplication.java
@@ -1,17 +1,26 @@
 package br.com.fiap.oneid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 
+import br.com.fiap.oneid.service.mqtt.MqttService;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableSwagger2
 public class OneidApplication extends SpringBootServletInitializer {
+	
+	@Autowired
+	private MqttService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneidApplication.class, args);
@@ -20,5 +29,12 @@ public class OneidApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(OneidApplication.class);
+	}
+	
+	@Bean
+	public IntegrationFlow mqttInbound() {
+		return IntegrationFlows
+				.from(new MqttPahoMessageDrivenChannelAdapter("tcp://broker.hivemq.com:1883", "client-oneid", "bgmbnewgen8462/oneid/empresa/request"))
+				.handle(m -> service.whatToDo(m.getPayload())).get();
 	}
 }
